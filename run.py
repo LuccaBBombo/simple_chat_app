72649613cd99cbc7092000890723acf0889eb942
Lucca Bombo <lucca.bombo@hotmail.com>
2020-05-11 17:32:17 +0000

    added Procfile for deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "random123"
+app.secret_key = os.getenv("SECRET", "randomkey")
 messages = []
 
 
@@ -33,4 +33,4 @@ def user(username):
 
     return render_template("chat.html", username = username, chat_messages = messages)
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
